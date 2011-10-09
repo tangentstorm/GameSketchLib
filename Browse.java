8fd38b88a15d8b4e98e4b6cf2e1ba515a6f472a2
sabren <michal.wallace@gmail.com>
2011-10-08 21:36:25 -0500

    added helper file for browsing

---
+++ Browse.java
@@ -0,0 +1,7 @@
+class Browse
+{
+	// Processing code is based on Java, so paste the code for a sketch in
+	// here and you'll (hopefully) be able to browse the structure in any
+	// decent Java IDE.
+	
+}
